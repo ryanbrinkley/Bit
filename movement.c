b45802a2c053bc115fac649c01eb6efce93e568f
ryanbrinkley <ryanbrinkley@yahoo.com>
2015-01-16 17:26:43 -0600

    Update movement.c

--- movement.c
+++ movement.c
@@ -6,13 +6,17 @@
 #define TWO_DIR		0x02
 #define ONE_DIR		0x01
 #define NO_DIR		0x00
-#define NORTH		1
-#define SOUTH		2
-#define EAST		3
-#define WEST		4//direction/...^^^
+
+#define NORTH		0         //		0
+#define SOUTH		2		  //	3		1
+#define EAST		1		  //		2				 
+#define WEST		3
+
 #define startr 		3//start row
 #define startc 		2//start column
+
 #define WALL		6//largest dist to wall
+#define SQUARE		12 //distance from one square to the next in inches
 
 struct ryans{
 	int noWallN;
@@ -21,9 +25,9 @@ struct ryans{
 	int noWallW;//north south east and west orientation.
 	int prev;
 	char opening;
-	
 };
 
+int facing = NORTH; //robot starts the maze facing NORTH
 struct ryans ryan[7][7];
 
 void init(){
@@ -78,8 +82,59 @@ void discover(int dir, int row, int col){
 	
 }
 
-int move(){
-return 0;
+//keep track of direction robot is facing
+//sends command to PSOC
+//faces the correct direction, then moves forward 12 inches
+//returns distance moved
+//
+//		0
+//	3		1
+//		2
+int move(int direction){
+	int i = facing - direction;
+	if(direction != facing){
+		//direction is 180 degrees from where the robot is facing
+		if(((facing-direction) % 4) == 2){
+			turnAround();
+		}
+		//direction is 90 degrees to the right of the robot
+		else if((facing - direction) < 0){
+			turnCW();
+		//direction is 90 degrees to the left of the robot
+		}else{
+			turnCCW();
+		}
+	}
+	moveForward();//advance to a new square, which is 12 inches ahead
+	
+	//should return 
+	return 0;
+}
+
+//turn 90 degrees right, then update facing variable 
+void turnCW(){	
+	
+	
+	facing = (facing+1) % 4;
+}
+
+//turn 90 degrees right, then update facing variable 
+void turnCCW(){
+	
+	
+	facing = (facing-1) % 4;
+}
+
+//turn 180 degrees, then update facing variable
+void turnAround(){
+	
+	
+	facing = (facing+2) % 4;
+}
+
+void moveForward(){ //move 12 inches forward
+	
+	
 }
 
 void isWall(int row, int col){
@@ -118,3 +173,4 @@ discover(0, 3,4);
 
 return 0;
 }
+
